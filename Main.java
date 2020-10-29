4f906719ba65708a309d22a41f536f41151e7955
fiscn <fiscn2709@wrdsb.ca>
2020-10-29 16:13:31 +0000

    Completed A8 Q1 program

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel; 
+
+  JLabel hiPerson; 
+
+  JTextField nameBox; 
+
+  JButton sayHelloButton; 
 
 
   // Method to assemble our GUI
@@ -17,7 +23,50 @@ public class Main implements Runnable, ActionListener{
     // makes the windows 800 pixel wide by 600 pixels tall
     frame.setSize(800,600);
     // shows the window
-    frame.setVisible(true);
+    frame.setVisible(true); 
+
+    //initalize the main panel 
+    mainPanel = new JPanel(); 
+    //diable layout helpers 
+    mainPanel.setLayout(null);  
+
+    //create text box for user 
+    nameBox = new JTextField(); 
+
+    //set location and size 
+    nameBox.setBounds(225,70,350,20); 
+
+    //initalize Button 
+    sayHelloButton = new JButton("Say Hello"); 
+
+    //set size and location of button 
+    sayHelloButton.setBounds(225,150,100,25); 
+
+    //add action listeners to the button 
+    sayHelloButton.addActionListener(this); 
+
+    //give the button an action command 
+    sayHelloButton.setActionCommand("Say hello"); 
+
+    //create Label to output answer to screen 
+    hiPerson = new JLabel(); 
+
+    //set size and location 
+    hiPerson.setBounds(225,200,100,25); 
+
+
+    //add label to window 
+    mainPanel.add(hiPerson); 
+
+
+    //add button to the window 
+    mainPanel.add(sayHelloButton);  
+
+    //add text field to the window 
+    mainPanel.add(nameBox);
+
+    //add panel to window 
+    frame.add(mainPanel); 
  
     
 
@@ -26,7 +75,14 @@ public class Main implements Runnable, ActionListener{
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
     // get the command from the action
-    String command = e.getActionCommand();
+    String command = e.getActionCommand(); 
+
+    if(command.equals("Say hello")){
+      //get the text from text box 
+      String userName = nameBox.getText(); 
+      //put the answer into the label 
+      hiPerson.setText("Hello " + userName);  
+    }
 
   }
 
